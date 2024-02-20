36a1f37881235aceea20c8f6d0ed0a85a5fb4fbf
Jean <jeanants@icloud.com>
2024-02-20 01:27:52 -0300

    starting with ts

---
+++ src/components/Comments/Comment.tsx
@@ -0,0 +1,56 @@
+import { ThumbsUp, Trash } from "phosphor-react"
+import style from "./Comment.module.css"
+import { Avatar } from "../Avatar/Avatar"
+import { useState } from "react"
+
+interface CommentsProps {
+    content: string;
+    onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentsProps) {
+
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleCommentDelete() {
+        onDeleteComment(content)
+        
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+    }
+
+    return (
+        <div className={style.Comment}>
+            <Avatar hasBorder={false} src="https://pbs.twimg.com/profile_images/1703502153229254656/evMDQcpo_400x400.jpg" alt="" />
+
+            <div className={style.CommentBox}>
+                <div className={style.CommentContent}>
+                    <header>
+                        <div className={style.AuthorAndTime}>
+                            <strong>Devon Lane</strong>
+
+                            <time title="February 15th at 11 am" dateTime="2024-02-15 09:59:23">Cerca de 1h atrás</time>
+
+                        </div>
+
+                        <button onClick={handleCommentDelete} title="Deletar Comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
